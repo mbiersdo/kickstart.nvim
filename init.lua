c1ec63520e1450b6fec2f47c22c4e27c06344534
Matt Biersdorf <matt.biersdorf@gmail.com>
2025-03-17 12:58:33 -0500

    Add mini.files plugin

--- init.lua
+++ init.lua
@@ -916,6 +916,34 @@ require('lazy').setup({
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
+  -- Mini files directory finder
+  {
+    'echasnovski/mini.files',
+    keys = {
+      {
+        '<leader>e',
+        function()
+          require('mini.files').open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = 'Open mini.files (directory of current file)',
+      },
+      {
+        '<leader>E',
+        function()
+          require('mini.files').open(vim.uv.cwd(), true)
+        end,
+        desc = 'Open mini.files (cwd)',
+      },
+      {
+        '<leader>fm',
+        function()
+          require('mini.files').open(LazyVim.root(), true)
+        end,
+        desc = 'Open mini.files (root)',
+      },
+    },
+  },
+
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
